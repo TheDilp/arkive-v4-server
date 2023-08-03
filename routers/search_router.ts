b5f7d3fcca6fa875f9cf795841f49504730476ac
= <=>
2023-08-03 07:51:47 +0200

    feat: mention search router

--- routers/search_router.ts
+++ routers/search_router.ts
@@ -3,7 +3,7 @@ import { SelectExpression } from "kysely";
 import { DB } from "kysely-codegen";
 
 import { db } from "../database/db";
-import { SearchableEntities } from "../types/requestTypes";
+import { SearchableEntities, SearchableMentionEntities } from "../types/requestTypes";
 
 export function search_router(server: FastifyInstance, _: any, done: any) {
   server.post(
@@ -51,6 +51,77 @@ export function search_router(server: FastifyInstance, _: any, done: any) {
         message: "Success",
         ok: true,
       });
+    },
+  );
+  server.post(
+    "/:project_id/:type/mentions",
+    async (
+      req: FastifyRequest<{
+        Params: { type: SearchableMentionEntities; project_id: string };
+        Body: {
+          data: {
+            search_term: string;
+          };
+          limit: number;
+        };
+      }>,
+      rep,
+    ) => {
+      const { type } = req.params;
+      const fields = ["id"];
+
+      if (type === "characters") fields.push("first_name", "nickname", "last_name", "portrait_id");
+      else fields.push("title");
+
+      if (type === "documents") {
+        const documents = await db
+          .selectFrom("documents")
+          .select(["id", "title"])
+          .where("title", "ilike", `%${req.body.data.search_term.toLowerCase()}%`)
+          .where("project_id", "=", req.params.project_id)
+          .limit(req.body.limit || 10)
+          .execute();
+        const alter_names = await db
+          .selectFrom("alter_names")
+          .select(["id as alterId", "title", "parent_id"])
+          .where("title", "ilike", `%${req.body.data.search_term.toLowerCase()}%`)
+          .where("project_id", "=", req.params.project_id)
+          .limit(req.body.limit || 10)
+          .execute();
+
+        const combinedResult = [...documents, ...alter_names];
+        rep.send({
+          data: combinedResult,
+          message: "Success",
+          ok: true,
+        });
+      }
+
+      // result = await db
+      //   .selectFrom(type)
+      //   .select(fields as SelectExpression<DB, SearchableEntities>[])
+      //   .where("project_id", "=", req.params.project_id)
+      //   .where((eb) =>
+      //     type === "characters"
+      //       ? eb.or([
+      //           eb("first_name", "ilike", `%${req.body.data.search_term}%`),
+      //           eb("nickname", "ilike", `%${req.body.data.search_term}%`),
+      //           eb("last_name", "ilike", `%${req.body.data.search_term}%`),
+      //         ])
+      //       : eb("title", "ilike", `%${req.body.data.search_term}%`),
+      //   )
+
+      //   .execute();
+      // rep.send({
+      //   data: result.map((item) => ({
+      //     value: item.id,
+      //     label: type === "characters" ? `${item.first_name} ${item?.last_name || ""}` : item?.title || "",
+      //     color: type === "tags" ? item.color : "",
+      //     image: type === "characters" ? item.portrait_id || "" : "",
+      //   })),
+      //   message: "Success",
+      //   ok: true,
+      // });
     },
   );
 
