be24349d064a114226c19a04269ee5b5f35b6c57
Mihailo Dilparic <mihailo.dilparic@gmail.com>
2023-09-08 11:48:52 +0200

    feat: ordering for events

--- routers/event_router.ts
+++ routers/event_router.ts
@@ -1,7 +1,12 @@
 import { FastifyInstance, FastifyRequest } from "fastify";
+import { SelectExpression } from "kysely";
+import { DB } from "kysely-codegen";
 
 import { db } from "../database/db";
 import { InsertEventSchema, InsertEventType } from "../database/validation";
+import { RequestBodyType } from "../types/requestTypes";
+import { constructFilter } from "../utils/filterConstructor";
+import { constructOrdering } from "../utils/orderByConstructor";
 
 export function event_router(server: FastifyInstance, _: any, done: any) {
   server.post(
@@ -25,6 +30,24 @@ export function event_router(server: FastifyInstance, _: any, done: any) {
       rep.send({ message: "Success.", ok: true });
     },
   );
+  server.post("/", async (req: FastifyRequest<{ Body: RequestBodyType }>, rep) => {
+    const data = await db
+      .selectFrom("events")
+
+      .$if(!req.body.fields?.length, (qb) => qb.selectAll())
+      .$if(!!req.body.fields?.length, (qb) => qb.clearSelect().select(req.body.fields as SelectExpression<DB, "events">[]))
+      .$if(!!req.body?.filters?.and?.length || !!req.body?.filters?.or?.length, (qb) => {
+        qb = constructFilter("events", qb, req.body.filters);
+        return qb;
+      })
+      .$if(!!req.body.orderBy?.length, (qb) => {
+        qb = constructOrdering(req.body.orderBy, qb);
+        return qb;
+      })
+      .execute();
+
+    rep.send({ data, message: "Success.", ok: true });
+  });
 
   done();
 }
