2c5228fd0593a4b50b1d4d2e944df795af1a8a12
Mihailo Dilparic <mihailo.dilparic@gmail.com>
2023-09-22 20:59:46 +0200

    fix: char fields on char create validation

--- database/validation/characters.ts
+++ database/validation/characters.ts
@@ -43,7 +43,9 @@ export const InsertCharacterSchema = t.Object({
   }),
   relations: t.Optional(
     t.Object({
-      character_fields: t.Optional(t.Array(t.Object({ id: t.String(), value: t.Optional(t.Union([t.String(), t.Null()])) }))),
+      character_fields: t.Optional(
+        t.Array(t.Object({ id: t.String(), value: t.Object({ value: t.Optional(t.Union([t.String(), t.Null()])) }) })),
+      ),
       related_to: t.Optional(t.Array(t.Object({ id: t.String(), relation_type: t.String() }))),
       related_from: t.Optional(t.Array(t.Object({ id: t.String(), relation_type: t.String() }))),
       tags: t.Optional(t.Array(t.Object({ id: t.String() }))),
