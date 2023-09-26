e58181c7383befabed4cf8203d7f682392c8b5a0
Mihailo Dilparic <mihailo.dilparic@gmail.com>
2023-09-26 08:54:22 +0200

    chore: change rel type validation

--- database/validation/relationship_types.ts
+++ database/validation/relationship_types.ts
@@ -1,10 +1,17 @@
 import { t } from "elysia";
 
+import { RequestBodySchema } from "../../types/requestTypes";
+
+export const ListCharacterRelationshipTypeSchema = t.Intersect([
+  RequestBodySchema,
+  t.Object({ data: t.Object({ project_id: t.String() }) }),
+]);
+
 export const InsertRelationshipTypeSchema = t.Object({
   data: t.Object({
     title: t.String(),
     project_id: t.String(),
-    ascendant_title: t.String(),
-    descendant_title: t.String(),
+    ascendant_title: t.Optional(t.Union([t.String(), t.Null()])),
+    descendant_title: t.Optional(t.Union([t.String(), t.Null()])),
   }),
 });
--- routers/character_relationship_types_router.ts
+++ routers/character_relationship_types_router.ts
@@ -1,19 +1,32 @@
 import Elysia from "elysia";
 
 import { db } from "../database/db";
-import { InsertRelationshipTypeSchema } from "../database/validation";
+import { InsertRelationshipTypeSchema, ListCharacterRelationshipTypeSchema } from "../database/validation";
 import { MessageEnum } from "../enums/requestEnums";
-import { ResponseSchema } from "../types/requestTypes";
+import { ResponseSchema, ResponseWithDataSchema } from "../types/requestTypes";
 
 export function character_relationship_types_router(app: Elysia) {
   return app.group("/character_relationship_types", (server) =>
-    server.post(
-      "/create",
-      async ({ body }) => {
-        await db.insertInto("character_relationship_types").values(body.data).execute();
-        return { message: `Relationship type ${MessageEnum.successfully_created}`, ok: true };
-      },
-      { body: InsertRelationshipTypeSchema, response: ResponseSchema },
-    ),
+    server
+      .post(
+        "/create",
+        async ({ body }) => {
+          await db.insertInto("character_relationship_types").values(body.data).execute();
+          return { message: `Relationship type ${MessageEnum.successfully_created}`, ok: true };
+        },
+        { body: InsertRelationshipTypeSchema, response: ResponseSchema },
+      )
+      .post(
+        "/",
+        async ({ body }) => {
+          const data = await db
+            .selectFrom("character_relationship_types")
+            .select(["id", "title", "ascendant_title", "descendant_title"])
+            .where("project_id", "=", body.data.project_id)
+            .execute();
+          return { data, message: MessageEnum.success, ok: true };
+        },
+        { body: ListCharacterRelationshipTypeSchema, respose: ResponseWithDataSchema },
+      ),
   );
 }
