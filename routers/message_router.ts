66e928ac303c5ba439941383798c1244c14e60e9
Mihailo Dilparic <mihailo@salestrekker.com>
2023-10-15 15:53:13 +0200

    feat: delete message route

--- routers/message_router.ts
+++ routers/message_router.ts
@@ -7,13 +7,24 @@ import { ResponseSchema } from "../types/requestTypes";
 
 export function message_router(app: Elysia) {
   return app.group("/messages", (server) =>
-    server.post(
-      "/create",
-      async ({ body }) => {
-        await db.insertInto("messages").values(body.data).execute();
-        return { message: MessageEnum.success, ok: true };
-      },
-      { body: InsertMessageSchema, response: ResponseSchema },
-    ),
+    server
+      .post(
+        "/create",
+        async ({ body }) => {
+          await db.insertInto("messages").values(body.data).execute();
+          return { message: MessageEnum.success, ok: true };
+        },
+        { body: InsertMessageSchema, response: ResponseSchema },
+      )
+      .delete(
+        "/:id",
+        async ({ params }) => {
+          await db.deleteFrom("messages").where("id", "=", params.id).execute();
+          return { message: `Message ${MessageEnum.successfully_deleted}`, ok: true };
+        },
+        {
+          response: ResponseSchema,
+        },
+      ),
   );
 }
