67f127f28c664af78eff59e6ac2b3f500e6a0c8c
Mihailo Dilparic <mihailo@salestrekker.com>
2023-10-16 18:38:51 +0200

    feat: read messages route

--- database/validation/messages.ts
+++ database/validation/messages.ts
@@ -1,5 +1,14 @@
 import { t } from "elysia";
 
+import { RequestBodySchema } from "../../types/requestTypes";
+
+export const ReadMessageSchema = t.Intersect([
+  RequestBodySchema,
+  t.Object({ data: t.Object({ id: t.String() }) }),
+
+  t.Optional(t.Object({ relations: t.Optional(t.Object({ character: t.Boolean() })) })),
+]);
+
 export const InsertMessageSchema = t.Object({
   data: t.Object({
     parent_id: t.String(),
--- routers/message_router.ts
+++ routers/message_router.ts
@@ -1,9 +1,12 @@
 import Elysia from "elysia";
+import { SelectExpression } from "kysely";
+import { jsonObjectFrom } from "kysely/helpers/postgres";
+import { DB } from "kysely-codegen";
 
 import { db } from "../database/db";
-import { InsertMessageSchema } from "../database/validation/messages";
+import { InsertMessageSchema, ReadMessageSchema } from "../database/validation/messages";
 import { MessageEnum } from "../enums/requestEnums";
-import { ResponseSchema } from "../types/requestTypes";
+import { ResponseSchema, ResponseWithDataSchema } from "../types/requestTypes";
 
 export function message_router(app: Elysia) {
   return app.group("/messages", (server) =>
@@ -15,6 +18,36 @@ export function message_router(app: Elysia) {
           return { message: MessageEnum.success, ok: true };
         },
         { body: InsertMessageSchema, response: ResponseSchema },
+      )
+      .post(
+        "/:id",
+        async ({ params, body }) => {
+          const data = await db
+            .selectFrom("messages")
+            .$if(!body.fields?.length, (qb) => qb.selectAll())
+            .$if(!!body.fields?.length, (qb) => qb.clearSelect().select(body.fields as SelectExpression<DB, "messages">[]))
+            .$if(!!body?.relations, (qb) => {
+              if (body?.relations?.character) {
+                qb = qb.select((eb) =>
+                  jsonObjectFrom(
+                    eb
+                      .selectFrom("characters")
+                      .whereRef("characters.id", "=", "messages.sender_id")
+                      .select(["characters.id", "characters.first_name", "characters.last_name", "characters.portrait_id"]),
+                  ).as("character"),
+                );
+              }
+              return qb;
+            })
+            .where("messages.id", "=", params.id)
+            .executeTakeFirstOrThrow();
+
+          return { data, message: MessageEnum.success, ok: true };
+        },
+        {
+          body: ReadMessageSchema,
+          response: ResponseWithDataSchema,
+        },
       )
       .delete(
         "/:id",
