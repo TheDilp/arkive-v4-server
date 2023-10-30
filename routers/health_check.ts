de5ffa4e50353f1f315677bb6f1193d5cf7c8639
Mihailo Dilparic <mihailo@salestrekker.com>
2023-10-30 21:12:48 +0100

    feat: add stytch client

--- routers/health_check.ts
+++ routers/health_check.ts
@@ -1,7 +1,24 @@
-import Elysia from "elysia";
+import Elysia, { t } from "elysia";
+
+import { authClient } from "../utils/stytchClient";
 
 export function health_check_router(app: Elysia) {
-  return app.get("/health_check", async () => {
-    return { basecheck: true, ok: true };
-  });
+  return app
+    .get("/health_check", async () => {
+      return { basecheck: true, ok: true };
+    })
+    .post(
+      "/password_strength",
+      async ({ body }) => {
+        authClient.passwords
+          .strengthCheck(body.data)
+          .then((resp) => {
+            console.log(resp);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      },
+      { body: t.Object({ data: t.Object({ password: t.String() }) }) },
+    );
 }
