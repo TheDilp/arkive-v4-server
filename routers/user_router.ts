66fbb50aa38fc74731b33e6f0db102d3e77f23c4
Mihailo Dilparic <mihailo@salestrekker.com>
2023-10-30 20:10:39 +0100

    feat: get user route

--- routers/user_router.ts
+++ routers/user_router.ts
@@ -1,20 +1,32 @@
 import Elysia from "elysia";
 
+import { db } from "../database/db";
 import { InsertUserSchema } from "../database/validation";
 import { MessageEnum } from "../enums/requestEnums";
-import { ResponseSchema } from "../types/requestTypes";
+import { ResponseSchema, ResponseWithDataSchema } from "../types/requestTypes";
 
 export function user_router(app: Elysia) {
   return app.group("/users", (server) =>
-    server.post(
-      "/create",
-      async () => {
-        return { message: MessageEnum.success, ok: true };
-      },
-      {
-        body: InsertUserSchema,
-        response: ResponseSchema,
-      },
-    ),
+    server
+      .post(
+        "/create",
+        async () => {
+          return { message: MessageEnum.success, ok: true };
+        },
+        {
+          body: InsertUserSchema,
+          response: ResponseSchema,
+        },
+      )
+      .get(
+        "/:id",
+        async ({ params }) => {
+          const data = await db.selectFrom("users").where("id", "=", params.id).executeTakeFirstOrThrow();
+          return { data, message: MessageEnum.success, ok: true };
+        },
+        {
+          response: ResponseWithDataSchema,
+        },
+      ),
   );
 }
