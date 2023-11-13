fef3445c93987e5ec3faf38f4d4baf8e02e11485
Mihailo Dilparic <mihailo@salestrekker.com>
2023-11-13 12:02:29 +0100

    fix: words validation

--- database/validation/words.ts
+++ database/validation/words.ts
@@ -4,14 +4,7 @@ import { RequestBodySchema } from "../../types/requestTypes";
 
 export const ListWordSchema = t.Intersect([RequestBodySchema, t.Object({ data: t.Object({ parent_id: t.String() }) })]);
 
-export const ReadWordSchema = t.Intersect([
-  RequestBodySchema,
-  t.Optional(
-    t.Object({
-      data: t.Object({ id: t.String() }),
-    }),
-  ),
-]);
+export const ReadWordSchema = t.Intersect([RequestBodySchema]);
 
 export const InserWordSchema = t.Object({
   data: t.Object({
