bea17b02e3c4dab8746bcfb13725ce7cbf7702d9
Mihailo Dilparic <mihailo@salestrekker.com>
2023-11-20 15:44:07 +0100

    fix: char add route validation

--- database/validation/characters.ts
+++ database/validation/characters.ts
@@ -128,84 +128,88 @@ export const InsertCharacterSchema = t.Object({
   ),
 });
 export const UpdateCharacterSchema = t.Object({
-  data: t.Object({
-    first_name: t.Optional(t.String()),
-    last_name: t.Optional(t.Union([t.String(), t.Null()])),
-    nickname: t.Optional(t.Union([t.String(), t.Null()])),
-    age: t.Optional(t.Union([t.Number(), t.Null()])),
-    portrait_id: t.Optional(t.Union([t.String(), t.Null()])),
-    map_pin_id: t.Optional(t.Union([t.String(), t.Null()])),
-    is_favorite: t.Optional(t.Union([t.Boolean(), t.Null()])),
-  }),
+  data: t.Optional(
+    t.Object({
+      first_name: t.Optional(t.String()),
+      last_name: t.Optional(t.Union([t.String(), t.Null()])),
+      nickname: t.Optional(t.Union([t.String(), t.Null()])),
+      age: t.Optional(t.Union([t.Number(), t.Null()])),
+      portrait_id: t.Optional(t.Union([t.String(), t.Null()])),
+      map_pin_id: t.Optional(t.Union([t.String(), t.Null()])),
+      is_favorite: t.Optional(t.Union([t.Boolean(), t.Null()])),
+    }),
+  ),
   relations: t.Optional(
     t.Object({
-      character_fields: t.Array(
-        t.Object({
-          id: t.String(),
+      character_fields: t.Optional(
+        t.Array(
+          t.Object({
+            id: t.String(),
 
-          blueprint_instances: t.Optional(
-            t.Array(
-              t.Object({
-                related_id: t.String(),
-              }),
+            blueprint_instances: t.Optional(
+              t.Array(
+                t.Object({
+                  related_id: t.String(),
+                }),
+              ),
             ),
-          ),
-          documents: t.Optional(
-            t.Array(
-              t.Object({
-                related_id: t.String(),
-              }),
+            documents: t.Optional(
+              t.Array(
+                t.Object({
+                  related_id: t.String(),
+                }),
+              ),
             ),
-          ),
-          map_pins: t.Optional(
-            t.Array(
-              t.Object({
-                related_id: t.String(),
-              }),
+            map_pins: t.Optional(
+              t.Array(
+                t.Object({
+                  related_id: t.String(),
+                }),
+              ),
             ),
-          ),
-          images: t.Optional(
-            t.Array(
-              t.Object({
-                related_id: t.String(),
-              }),
+            images: t.Optional(
+              t.Array(
+                t.Object({
+                  related_id: t.String(),
+                }),
+              ),
             ),
-          ),
-          random_table: t.Optional(
-            t.Union([
-              t.Object({
-                option_id: t.Optional(t.Union([t.Null(), t.String()])),
-                suboption_id: t.Optional(t.Union([t.Null(), t.String()])),
-                related_id: t.String(),
-              }),
-              t.Null(),
-            ]),
-          ),
-          calendar: t.Optional(
-            t.Union([
-              t.Object({
-                start_day: t.Optional(t.Union([t.Number(), t.Null()])),
-                start_year: t.Optional(t.Union([t.Number(), t.Null()])),
-                start_month_id: t.Optional(t.Union([t.String(), t.Null()])),
-                end_day: t.Optional(t.Union([t.Number(), t.Null()])),
-                end_month_id: t.Optional(t.Union([t.String(), t.Null()])),
-                end_year: t.Optional(t.Union([t.Number(), t.Null()])),
-                related_id: t.String(),
-              }),
-              t.Null(),
-            ]),
-          ),
-          value: t.Optional(
-            t.Union([
-              t.Array(t.Union([t.String(), t.Number(), t.Null()])),
-              t.String(),
-              t.Number(),
-              t.Null(),
-              t.Boolean(),
-              t.Record(t.String(), t.Any()),
-            ]),
-          ),
-        }),
+            random_table: t.Optional(
+              t.Union([
+                t.Object({
+                  option_id: t.Optional(t.Union([t.Null(), t.String()])),
+                  suboption_id: t.Optional(t.Union([t.Null(), t.String()])),
+                  related_id: t.String(),
+                }),
+                t.Null(),
+              ]),
+            ),
+            calendar: t.Optional(
+              t.Union([
+                t.Object({
+                  start_day: t.Optional(t.Union([t.Number(), t.Null()])),
+                  start_year: t.Optional(t.Union([t.Number(), t.Null()])),
+                  start_month_id: t.Optional(t.Union([t.String(), t.Null()])),
+                  end_day: t.Optional(t.Union([t.Number(), t.Null()])),
+                  end_month_id: t.Optional(t.Union([t.String(), t.Null()])),
+                  end_year: t.Optional(t.Union([t.Number(), t.Null()])),
+                  related_id: t.String(),
+                }),
+                t.Null(),
+              ]),
+            ),
+            value: t.Optional(
+              t.Union([
+                t.Array(t.Union([t.String(), t.Number(), t.Null()])),
+                t.String(),
+                t.Number(),
+                t.Null(),
+                t.Boolean(),
+                t.Record(t.String(), t.Any()),
+              ]),
+            ),
+          }),
+        ),
       ),
       related_to: t.Optional(
         t.Array(
