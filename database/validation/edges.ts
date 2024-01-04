ceb95aeeb8716a88ca594218ff40c05509be4e38
Mihailo Dilparic <mihailo@salestrekker.com>
2024-01-04 08:32:26 +0100

    fix: edge validation

--- database/validation/edges.ts
+++ database/validation/edges.ts
@@ -2,13 +2,16 @@ import { t } from "elysia";
 
 import { RequestBodySchema } from "../../types/requestTypes";
 
-export const ReadEdgeSchema = t.Object({
-  relations: t.Optional(
-    t.Object({
-      tags: t.Optional(t.Boolean()),
-    }),
-  ),
-});
+export const ReadEdgeSchema = t.Intersect([
+  RequestBodySchema,
+  t.Object({
+    relations: t.Optional(
+      t.Object({
+        tags: t.Optional(t.Boolean()),
+      }),
+    ),
+  }),
+]);
 export const ListEdgesSchema = t.Intersect([RequestBodySchema, t.Object({ data: t.Object({ parent_id: t.String() }) })]);
 
 export const InsertEdgeSchema = t.Object({
