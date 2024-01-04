b2cdb1d30f8fa7cb1b365160b0d36c59a5eacb7d
Mihailo Dilparic <mihailo@salestrekker.com>
2024-01-04 08:37:26 +0100

    fix: nodes validation

--- database/validation/nodes.ts
+++ database/validation/nodes.ts
@@ -28,18 +28,21 @@ export const NodeShapeEnum = t.Optional(
   ]),
 );
 
-export const ReadNodeSchema = t.Object({
-  relations: t.Optional(
-    t.Object({
-      tags: t.Optional(t.Boolean()),
-      image: t.Optional(t.Boolean()),
-      character: t.Optional(t.Boolean()),
-      document: t.Optional(t.Boolean()),
-      map_pin: t.Optional(t.Boolean()),
-      event: t.Optional(t.Boolean()),
-    }),
-  ),
-});
+export const ReadNodeSchema = t.Intersect([
+  RequestBodySchema,
+  t.Object({
+    relations: t.Optional(
+      t.Object({
+        tags: t.Optional(t.Boolean()),
+        image: t.Optional(t.Boolean()),
+        character: t.Optional(t.Boolean()),
+        document: t.Optional(t.Boolean()),
+        map_pin: t.Optional(t.Boolean()),
+        event: t.Optional(t.Boolean()),
+      }),
+    ),
+  }),
+]);
 
 export const ListNodesSchema = t.Intersect([RequestBodySchema, t.Object({ data: t.Object({ parent_id: t.String() }) })]);
 
--- routers/edge_router.ts
+++ routers/edge_router.ts
@@ -156,24 +156,6 @@ export function edge_router(app: Elysia) {
       .delete("/:id", async ({ params }) => {
         await db.deleteFrom("edges").where("edges.id", "=", params.id).execute();
         return { message: `Edge ${MessageEnum.successfully_deleted}`, ok: true };
-      })
-      .delete(
-        "/",
-        async ({ body }) => {
-          await db
-            .deleteFrom("edges")
-            .where(
-              "edges.id",
-              "=",
-              body.data.map((i) => i.id),
-            )
-            .execute();
-          return { message: `Edges ${MessageEnum.successfully_deleted}`, ok: true };
-        },
-        {
-          body: DeleteManyEdgeSchema,
-          response: ResponseSchema,
-        },
-      ),
+      }),
   );
 }
