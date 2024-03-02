57f6d53afcec5af34632b8bf7dfd70737b41a857
Mihailo Dilparic <mihailo@salestrekker.com>
2024-03-02 14:34:55 +0100

    chore: add logging

--- routers/meta_router.ts
+++ routers/meta_router.ts
@@ -4,6 +4,8 @@ export function meta_router(app: Elysia) {
   return app
     .group("/meta", (server) => server.post("/version/:type", () => {}))
     .onStart(async () => {
-      app.server.publish("version", JSON.stringify({ timestamp: new Date().getTime() }));
+      const timestamp = new Date().getTime();
+      console.log(timestamp);
+      app.server.publish("version", JSON.stringify({ timestamp }));
     });
 }
