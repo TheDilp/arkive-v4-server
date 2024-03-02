c4652635e137362d01d697d960305178e0c21f29
Mihailo Dilparic <mihailo@salestrekker.com>
2024-03-02 14:48:35 +0100

    fix: notification for deploys

--- routers/meta_router.ts
+++ routers/meta_router.ts
@@ -1,11 +1,22 @@
-import Elysia from "elysia";
+import Elysia, { t } from "elysia";
 
 export function meta_router(app: Elysia) {
-  return app
-    .group("/meta", (server) => server.post("/version/:type", () => {}))
-    .onStart(async () => {
-      const timestamp = new Date().getTime();
-      console.log(timestamp);
-      app.server.publish("version", JSON.stringify({ timestamp }));
-    });
+  return app.group("/meta", (server) =>
+    server.post(
+      "/version",
+      ({ headers, body }) => {
+        console.log(headers);
+        if (headers.origin === "https://railway.app") {
+          if (body.type === "DEPLOY" && body?.status === "SUCCESS") {
+            const timestamp = new Date().getTime();
+            app.server.publish("version", JSON.stringify({ timestamp }));
+          }
+          return true;
+        }
+      },
+      {
+        body: t.Object({ type: t.Literal("DEPLOY"), status: t.String() }, { additionalProperties: true }),
+      },
+    ),
+  );
 }
