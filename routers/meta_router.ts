ccff9c1906bef71ec4fa514fde138a254c095596
Mihailo Dilparic <mihailo@salestrekker.com>
2024-03-02 14:11:44 +0100

    feat: add github webhook notification

--- routers/meta_router.ts
+++ routers/meta_router.ts
@@ -1,18 +1,13 @@
-import Elysia, { t } from "elysia";
+import Elysia from "elysia";
+
+import { verify_signature } from "../utils/requestUtils";
 
 export function meta_router(app: Elysia) {
   return app.group("/meta", (server) =>
-    server.post(
-      "/version",
-      ({ body, headers }) => {
-        if (headers.origin === "https://railway.app") {
-          console.log(headers.origin, body.timestamp);
-          app.server.publish("version", JSON.stringify({ timestamp: body.timestamp }));
-        }
-      },
-      {
-        body: t.Object({ timestamp: t.String() }, { additionalProperties: true }),
-      },
-    ),
+    server.post("/version/:type", ({ params, body, headers }) => {
+      if (verify_signature(body, headers["x-hub-signature-256"] as string, params.type as "front" | "back")) {
+        app.server.publish("version", JSON.stringify({ timestamp: new Date().getTime() }));
+      }
+    }),
   );
 }
