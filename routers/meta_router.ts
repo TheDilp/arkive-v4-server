cf7f3203733f8cc2538a256d865cf17269dbfa9e
Mihailo Dilparic <mihailo@salestrekker.com>
2024-03-02 14:15:47 +0100

    chore: add branch condition for github webhooks

--- routers/meta_router.ts
+++ routers/meta_router.ts
@@ -1,13 +1,22 @@
-import Elysia from "elysia";
+import Elysia, { t } from "elysia";
 
 import { verify_signature } from "../utils/requestUtils";
 
 export function meta_router(app: Elysia) {
   return app.group("/meta", (server) =>
-    server.post("/version/:type", ({ params, body, headers }) => {
-      if (verify_signature(body, headers["x-hub-signature-256"] as string, params.type as "front" | "back")) {
-        app.server.publish("version", JSON.stringify({ timestamp: new Date().getTime() }));
-      }
-    }),
+    server.post(
+      "/version/:type",
+      ({ params, body, headers }) => {
+        if (body.ref.endsWith("main")) {
+          if (verify_signature(body, headers["x-hub-signature-256"] as string, params.type as "front" | "back")) {
+            app.server.publish("version", JSON.stringify({ timestamp: new Date().getTime() }));
+          }
+        }
+        return true;
+      },
+      {
+        body: t.Object({ ref: t.String() }, { additionalProperties: true }),
+      },
+    ),
   );
 }
