e92f80f2ce181ce74c8104b086adbb85859840d0
Mihailo Dilparic <mihailo@salestrekker.com>
2024-03-02 14:50:22 +0100

    chore: fix conditional

--- routers/meta_router.ts
+++ routers/meta_router.ts
@@ -6,13 +6,11 @@ export function meta_router(app: Elysia) {
       "/version",
       ({ headers, body }) => {
         console.log(headers);
-        if (headers.origin === "https://railway.app") {
-          if (body.type === "DEPLOY" && body?.status === "SUCCESS") {
-            const timestamp = new Date().getTime();
-            app.server.publish("version", JSON.stringify({ timestamp }));
-          }
-          return true;
+        if (body.type === "DEPLOY" && body?.status === "SUCCESS") {
+          const timestamp = new Date().getTime();
+          app.server.publish("version", JSON.stringify({ timestamp }));
         }
+        return true;
       },
       {
         body: t.Object({ type: t.Literal("DEPLOY"), status: t.String() }, { additionalProperties: true }),
