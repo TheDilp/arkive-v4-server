ecb1037386a0b7a5b9650e99a17593d01858f754
Mihailo Dilparic <mihailo@salestrekker.com>
2024-03-04 17:23:39 +0100

    fix: search parent_id can be null

--- database/validation/search.ts
+++ database/validation/search.ts
@@ -1,7 +1,7 @@
 import { t } from "elysia";
 
 export const BasicSearchSchema = t.Object({
-  data: t.Object({ search_term: t.String(), project_id: t.String(), parent_id: t.Optional(t.String()) }),
+  data: t.Object({ search_term: t.String(), project_id: t.String(), parent_id: t.Optional(t.Union([t.Null(), t.String()])) }),
   limit: t.Optional(t.Number()),
 });
 
