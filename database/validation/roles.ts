7d36323af15d37cc1352fd07d809ebefd374cd09
Mihailo Dilparic <mihailo@salestrekker.com>
2024-03-06 15:38:13 +0100

    feat: list roles route

--- database/validation/roles.ts
+++ database/validation/roles.ts
@@ -1,5 +1,17 @@
 import { t } from "elysia";
 
+import { RequestBodySchema } from "../../types/requestTypes";
+
+export const ListRoleSchema = t.Intersect([
+  RequestBodySchema,
+  t.Optional(t.Object({ relations: t.Optional(t.Object({ permissions: t.Optional(t.Boolean()) })) })),
+  t.Object({
+    data: t.Object({
+      project_id: t.String(),
+    }),
+  }),
+]);
+
 export const InsertRoleSchema = t.Object({
   data: t.Object({
     title: t.String(),
--- routers/role_router.ts
+++ routers/role_router.ts
@@ -1,34 +1,66 @@
 import Elysia from "elysia";
+import { jsonArrayFrom } from "kysely/helpers/postgres";
 
 import { db } from "../database/db";
-import { InsertRoleSchema } from "../database/validation";
+import { InsertRoleSchema, ListRoleSchema } from "../database/validation";
 import { MessageEnum } from "../enums/requestEnums";
-import { ResponseSchema } from "../types/requestTypes";
+import { ResponseSchema, ResponseWithDataSchema } from "../types/requestTypes";
 
 export function role_router(app: Elysia) {
   return app.group("/roles", (server) =>
-    server.post(
-      "/create",
-      async ({ body }) => {
-        await db.transaction().execute(async (tx) => {
-          const newRole = await tx
-            .insertInto("roles")
-            .values({ title: body.data.title, project_id: body.data.project_id })
-            .returning("id")
-            .executeTakeFirstOrThrow();
+    server
+      .post(
+        "/create",
+        async ({ body }) => {
+          await db.transaction().execute(async (tx) => {
+            const newRole = await tx
+              .insertInto("roles")
+              .values({ title: body.data.title, project_id: body.data.project_id })
+              .returning("id")
+              .executeTakeFirstOrThrow();
 
-          return await tx
-            .insertInto("role_permissions")
-            .values(body.data.permissions.map((perm) => ({ role_id: newRole.id, permission_id: perm })))
+            return await tx
+              .insertInto("role_permissions")
+              .values(body.data.permissions.map((perm) => ({ role_id: newRole.id, permission_id: perm })))
+              .execute();
+          });
+
+          return { message: `Role ${MessageEnum.successfully_created}`, ok: true };
+        },
+        {
+          body: InsertRoleSchema,
+          response: ResponseSchema,
+        },
+      )
+      .post(
+        "/",
+        async ({ body }) => {
+          const data = await db
+            .selectFrom("roles")
+            .where("project_id", "=", body.data.project_id)
+            .select(["id", "title"])
+            .$if(!!body.relations?.permissions, (qb) => {
+              qb = qb.select([
+                (eb) =>
+                  jsonArrayFrom(
+                    eb
+                      .selectFrom("role_permissions")
+                      .whereRef("role_id", "=", "roles.id")
+                      .leftJoin("permissions", "role_permissions.permission_id", "permissions.id")
+                      .select(["permissions.id", "permissions.title"]),
+                  ).as("permissions"),
+              ]);
+
+              return qb;
+            })
             .execute();
-        });
 
-        return { message: `Role ${MessageEnum.successfully_created}`, ok: true };
-      },
-      {
-        body: InsertRoleSchema,
-        response: ResponseSchema,
-      },
-    ),
+          return { data, message: MessageEnum.success, ok: true };
+        },
+        {
+          body: ListRoleSchema,
+          response: ResponseWithDataSchema,
+        },
+      ),
   );
 }
