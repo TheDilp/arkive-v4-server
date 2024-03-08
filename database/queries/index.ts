e5e8e21f800b4c7c610e82c1aeb501729c9fac94
Mihailo Dilparic <mihailo@salestrekker.com>
2024-03-08 11:29:12 +0100

    feat: entity ownership check abstract fn

--- database/queries/index.ts
+++ database/queries/index.ts
@@ -1,1 +1,2 @@
 export * from "./characterQueries";
+export * from "./ownershipCheck";
---
+++ database/queries/ownershipCheck.ts
@@ -0,0 +1,28 @@
+import { SelectQueryBuilder } from "kysely";
+
+import { EntitiesWithPermissionCheck } from "../../types/entityTypes";
+import { PermissionDecorationType } from "../../types/requestTypes";
+import { getPermissionTableFromEntity } from "../../utils/requestUtils";
+
+export function checkEntityLevelPermission(
+  qb: SelectQueryBuilder<any, any, any>,
+  permissions: PermissionDecorationType,
+  entity: EntitiesWithPermissionCheck,
+) {
+  const entityRelationTable = getPermissionTableFromEntity(entity);
+  qb = qb.where((wb) =>
+    wb.or([
+      wb(`${entity}.owner_id`, "=", permissions.user_id),
+      wb.and([
+        wb(`${entityRelationTable}.user_id`, "=", permissions.user_id),
+        wb(`${entityRelationTable}.permission_id`, "=", permissions.permission_id),
+        wb(`${entityRelationTable}.related_id`, "=", wb.ref(`${entity}.id`)),
+      ]),
+      wb.and([
+        wb(`${entityRelationTable}.user_id`, "=", permissions.user_id),
+        wb(`${entityRelationTable}.role_id`, "=", permissions.role_id),
+      ]),
+    ]),
+  );
+  return qb;
+}
