88a4e346542e01a4d0e03078b7a96d30f90f0c9c
Mihailo <mihailo.dilparic@gmail.com>
2024-07-19 20:20:59 +0200

    chore: disable aot

--- index.ts
+++ index.ts
@@ -49,7 +49,7 @@ import {
 } from "./routers";
 import { getCookieExpiry, verifyJWT } from "./utils/userUtils";
 
-export const app = new Elysia({ name: "Editor.Router" })
+export const app = new Elysia({ name: "Editor.Router", aot: false })
   .error({
     UNAUTHORIZED: UnauthorizedError,
     NO_PUBLIC_ACCESS: NoPublicAccess,
