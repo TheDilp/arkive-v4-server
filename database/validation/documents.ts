04f8e7db5a448f074f109a4dc18c51ac67b34e38
Mihailo <mihailo.dilparic@gmail.com>
2024-07-22 22:04:53 +0200

    fix: alter names update

--- database/validation/documents.ts
+++ database/validation/documents.ts
@@ -133,7 +133,6 @@ export const UpdateDocumentSchema = t.Object({
       alter_names: t.Optional(
         t.Array(
           t.Object({
-            id: t.String(),
             title: t.String(),
           }),
         ),
