46b023ae2e4dfaaa041f259d10ad0423d6de29b0
Mihailo <mihailo.dilparic@gmail.com>
2024-09-08 08:55:09 +0200

    chore: make player id required

--- database/migrations/20240907104211_use_players_instead_of_users.sql
+++ database/migrations/20240907104211_use_players_instead_of_users.sql
@@ -3,7 +3,7 @@ ALTER TABLE IF EXISTS game_character_permissions
 DROP COLUMN user_id;
 
 ALTER TABLE IF EXISTS game_character_permissions
-ADD COLUMN player_id UUID REFERENCES game_players (id) ON DELETE CASCADE;
+ADD COLUMN player_id UUID NOT NULL REFERENCES game_players (id) ON DELETE CASCADE;
 
 ALTER TABLE IF EXISTS game_character_permissions
 ADD COLUMN permission TEXT NOT NULL DEFAULT 'none' CHECK (permission IN ('none', 'view', 'read', 'own'))
