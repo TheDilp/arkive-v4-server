c5df111a8531ea842b983b0b263c7b7b88e1db55
Mihailo <mihailo.dilparic@gmail.com>
2024-09-15 10:46:27 +0200

    feat: parent_id for game characters

---
+++ database/migrations/20240915081034_add_parent_id_to_game_characters.sql
@@ -0,0 +1,7 @@
+-- migrate:up
+ALTER TABLE IF EXISTS game_characters
+ADD COLUMN parent_id REFERENCES game_characters (id) ON DELETE CASCADE;
+
+-- migrate:down
+ALTER TABLE IF EXISTS game_characters
+DROP COLUMN parent_id;
