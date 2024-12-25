883358ab3b06383f5c3c41fa61fe21d26265356a
Mihailo <mihailo.dilparic@gmail.com>
2024-12-25 13:50:57 +0100

    fix: clear cookie

--- routers/auth_router.ts
+++ routers/auth_router.ts
@@ -49,7 +49,7 @@ export function auth_router(app: Elysia) {
           method: "GET",
         });
         // @ts-ignore
-        set.headers["set-cookie"] = res.headers["set-cookie"];
+        set.headers["set-cookie"] = res.headers.get("set-cookie");
         set.status = res.status;
 
         return "UNAUTHORIZED";
